8f920175087f05e7771c11e619175c563a3ab04c
Antsinar <tsinaridisan@gmail.com>
2025-03-11 18:42:43 +0200

    Implemented basic sql queries for leaderboard objects

--- src/storage.py
+++ src/storage.py
@@ -1,15 +1,27 @@
-import sqlite3
+import asyncio
+import inspect
+import logging
+from contextlib import contextmanager
 from enum import Enum
 from functools import cached_property
-from typing import Dict, List, Protocol
+from queue import Queue
+from typing import Dict, Generator, List, Protocol
 from uuid import uuid4
 
 import orjson
 from pydantic import BaseModel, Field, ValidationError
 from pymemcache.client.base import PooledClient
 from pymemcache.exceptions import MemcacheError
+from sqlalchemy import Engine, create_engine, event, text
+from sqlalchemy.orm import sessionmaker
 
 from .models import CourseComplete, CourseModifiersHidden, CourseTracker
+
+logger = logging.getLogger(__name__)
+
+
+class DatabaseBusyError(Exception):
+    pass
 
 
 class StorageEngine(Enum):
@@ -43,6 +55,10 @@ class LeaderboardComplete(CourseComplete):
 
 
 class ILeaderboardRepository(Protocol):
+    def backup(self, path: str):
+        """Backup the current state of the repository to permanent storage"""
+        ...
+
     def init_leaderboard(self, course_url: str, moves: int) -> None:
         """Create a new leaderboard for a course url and move combination"""
         ...
@@ -98,6 +114,9 @@ class DictLeaderboardRepository:
     def __init__(self):
         self.leaderboards: Dict[str, List[LeaderboardDisplay]] = dict()
         self.trackers: Dict[str, LeaderboardTracker] = dict()
+
+    def backup(self, path: str) -> None:
+        pass
 
     def init_leaderboard(self, course_url: str, moves: int) -> None:
         leaderboard_key = LeaderboardName(course_url=course_url, moves=moves).key
@@ -197,8 +216,251 @@ class DictLeaderboardRepository:
 
 
 class SQLiteLeaderboardRepository:
-    def __init__(self):
-        pass
+    def __init__(self, database_uri: str):
+        self.database_uri = database_uri
+        self.engine = create_engine(self.database_uri)
+        self.tracker_queue: Queue[LeaderboardComplete] = Queue()
+        self.busy = False
+        self.loop = asyncio.get_running_loop()
+        self.loop.run_in_executor(self._watch_tracker_queue())
+
+    @event.listens_for(Engine, "connect", once=True)
+    def _set_pragma(self) -> None:
+        with self._flag_busy():
+            connection = self.engine.connect()
+            connection.execute(text("PRAGMA foreign_keys=ON"))
+            connection.execute(text("PRAGMA JOURNAL_MODE=WAL"))
+            connection.commit()
+            connection.close()
+
+    @contextmanager
+    def _flag_busy(self) -> Generator[None, None, None]:
+        try:
+            if self.busy:
+                raise DatabaseBusyError("Database is currently busy")
+            self.busy = True
+            logger.info("Database write lock aquired")
+            yield
+            self.busy = False
+            logger.info("Database write lock released")
+        except DatabaseBusyError as dbe:
+            logger.error(dbe)
+
+    def backup(self, path: str) -> None:
+        with self._flag_busy():
+            connection = self.engine.connect()
+            connection.execute(text(".backup :path"), {"path": path})
+            connection.close()
+
+    def init_leaderboard(self, course_url: str, moves: int) -> None:
+        with sessionmaker(self.engine)() as session:
+            session.execute(
+                text(
+                    """INSERT INTO leaderboard (course_url, moves)
+                       VALUES (:course_url, :moves)
+                       ON CONFLICT DO NOTHING
+                    """
+                ),
+                {"course_url": course_url, "moves": moves},
+            )
+            # TODO: update index here
+            session.commit()
+
+    def query_leaderboard(
+        self, course_url: str, max_moves: int, start: int = 0, limit: int | None = 100
+    ) -> List[LeaderboardDisplay]:
+        with sessionmaker(self.engine)() as session:
+            entries = session.execute(
+                text(
+                    """SELECT uid, nickname, score, course_uid, stamp FROM leaderboard_display display
+                       INNER JOIN leaderboard
+                       ON leaderboard_display.leaderboard_id = leaderboard.id
+                       WHERE leaderboard.course_url = :course_url AND leaderboard.moves = :moves
+                       ORDER BY display.score DESC
+                       LIMIT :limit OFFSET :start
+                    """
+                ),
+                {
+                    "course_url": course_url,
+                    "moves": max_moves,
+                    "start": start,
+                    "limit": limit,
+                },
+            )
+            result = entries.fetchall()
+            if not result:
+                return list()
+            display_keys = inspect.signature(LeaderboardDisplay).parameters.keys()
+            return [
+                LeaderboardDisplay(**{el[0]: el[1] for el in zip(display_keys, entry)})
+                for entry in result
+            ]
+
+    def drop_leaderboard(self, course_url: str, max_moves: int) -> None:
+        with sessionmaker(self.engine)() as session:
+            result = session.execute(
+                text(
+                    """DELETE FROM leaderboard
+                       WHERE course_url = :course_url
+                       AND moves = :moves
+                       RETURNING uid
+                       """
+                ),
+                {"course_url": course_url, "moves": max_moves},
+            )
+            ids = result.fetchall()
+            if not ids:
+                logger.error(
+                    "Leaderboard with course url %s and moves %s not found"
+                    % (course_url, max_moves)
+                )
+                session.rollback()
+            session.commit()
+
+    def invalidate(self, entry_id) -> None:
+        raise NotImplementedError
+
+    def update_leaderboard(
+        self, course_url: str, max_moves: int, entry: LeaderboardDisplay
+    ) -> None:
+        with sessionmaker(self.engine)() as session:
+            result = session.execute(
+                text(
+                    """INSERT INTO leaderboard_display
+                       (leaderboard_uid, uid, nickname, score, course_uid, stamp)
+                       VALUES (
+                        (SELECT uid FROM leaderboard WHERE course_url = :course_url AND moves = :moves),
+                        :uid, :nickname, :score, :course_uid, :stamp
+                       )
+                       RETURNING uid
+                    """
+                ),
+                {
+                    "course_url": course_url,
+                    "moves": max_moves,
+                    "uid": entry.uid,
+                    "nickname": entry.nickname,
+                    "score": entry.score,
+                    "course_uid": entry.course_uid,
+                    "stamp": entry.stamp,
+                },
+            )
+            entry_id = result.fetchone()
+            if not entry_id:
+                logger.error(f"Failed to insert {entry}")
+                session.rollback()
+            session.commit()
+
+    def course_exists(self, course_url: str, max_moves: int, course_uid: str) -> bool:
+        with sessionmaker(self.engine)() as session:
+            entries = session.execute(
+                text(
+                    """SELECT course_uid FROM leaderboard_display display
+                       INNER JOIN leaderboard
+                       ON leaderboard_display.leaderboard_uid = leaderboard.uid
+                       WHERE leaderboard.course_url = :course_url AND leaderboard.moves = :moves
+                    """
+                ),
+                {
+                    "course_url": course_url,
+                    "moves": max_moves,
+                },
+            )
+            result = entries.fetchall()
+            return course_uid in [el[0] for el in result]
+
+    def queue_tracker_object(self, entry: LeaderboardComplete) -> None:
+        self.tracker_queue.put(entry)
+
+    def write_tracker_object(self, entry: LeaderboardComplete) -> None:
+        if not self.course_exists(
+            entry.url, entry.tracker.move_tracker.moves_target, entry.uid
+        ):
+            logger.error("Course with uid %s not found", entry.uid)
+            return
+
+        with self._flag_busy():
+            with sessionmaker(self.engine)() as session:
+                result = session.execute(
+                    text(
+                        """INSERT INTO leaderboard_tracker
+                          (uid, data)
+                          VALUES (:uid, :data)
+                          RETURNING uid
+                        """
+                    ),
+                    {"uid": uuid4().hex, "data": entry.tracker.model_dump_json()},
+                )
+                if not result.fetchone():
+                    logger.error(
+                        f"Failed to insert tracker object for course {entry.uid}"
+                    )
+                    session.rollback()
+                session.commit()
+
+    def read_tracker_object(self, course_id: str) -> LeaderboardTracker | None:
+        with sessionmaker(self.engine)() as session:
+            tracker = session.execute(
+                text(
+                    """SELECT data FROM leaderboard_tracker tracker
+                       INNER JOIN leaderboard_display display
+                       ON tracker.uid = display.tracker_uid
+                       WHERE display.course_uid = :course_uid
+                    """
+                ),
+                {"course_uid": course_id},
+            )
+            result = tracker.fetchone()
+            if not result:
+                return None
+            try:
+                return LeaderboardTracker(**orjson.loads(result[1]))
+            except orjson.JSONDecodeError:
+                return None
+
+    def query_course_trackers(
+        self, course_url: str, max_moves: int, start: int = 0, limit: int | None = 100
+    ) -> List[LeaderboardTracker]:
+        with sessionmaker(self.engine)() as session:
+            trackers = session.execute(
+                text(
+                    """SELECT data FROM leaderboard_tracker tracker
+                       INNER JOIN leaderboard_display display
+                       ON tracker.uid = display.tracker_uid
+                       INNER JOIN leaderboard
+                       ON display.leaderboard_uid = leaderboard.uid
+                       WHERE leaderboard.course_url = :course_url
+                       AND leaderboard.moves = :moves
+                       ORDER BY display.score DESC
+                       LIMIT :limit OFFSET :start
+                    """
+                ),
+                {
+                    "course_url": course_url,
+                    "moves": max_moves,
+                    "start": start,
+                    "limit": limit,
+                },
+            )
+            results = trackers.fetchall()
+            if not results:
+                return list()
+            try:
+                return [LeaderboardTracker(**orjson.loads(el[0])) for el in results]
+            except orjson.JSONDecodeError:
+                logger.error("Failed to decode tracker object")
+                return list()
+
+    def delete_tracker_object(self, course_id: str) -> None:
+        raise NotImplementedError
+
+    def _watch_tracker_queue(self):
+        while True:
+            if self.tracker_queue.empty() or self.busy:
+                continue
+            entry = self.tracker_queue.get()
+            self.write_tracker_object(entry)
+            self.tracker_queue.task_done()
 
 
 def _match_engine(engine: StorageEngine) -> ILeaderboardRepository:
